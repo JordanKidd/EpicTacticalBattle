a4cce373569dd9b41a504a2ad84793408a6d622f
Jordan Kidd <jordankidd14@gmail.com>
2014-11-01 12:39:30 -0400

    Added some click logic on falling box

---
+++ unity/FFTClone/Assets/ClickAndMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAndMove : MonoBehaviour {
+
+	int numberOfClicks;
+	int amountToMove;
+
+	// Use this for initialization
+	void Start () {
+		numberOfClicks = 0;
+	}
+
+	void OnMouseDown() {
+		numberOfClicks++;
+		Debug.Log ("Clicked on box. Count is: " + numberOfClicks);
+		if (numberOfClicks % 2 == 0) {
+			Debug.Log("Moving down.");
+			amountToMove = 1
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
